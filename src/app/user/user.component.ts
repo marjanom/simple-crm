549088f278b434e21c3e7cba49035253dca1f509
Crunck78 <crunck78@googlemail.com>
2022-03-28 10:29:40 +0200

    update ng

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,7 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from 'src/models/user.class';
 import { firestore } from 'firebase';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, QueryFn } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
@@ -18,10 +18,14 @@ export class UserComponent implements OnInit {
   constructor(public dialog: MatDialog,
      private firestore: AngularFirestore) { }
 
+  filterFirstName(ref) : QueryFn{
+     return ref.orderBy('firstName', 'asc');
+  }
+
   ngOnInit(): void {
 
     this.firestore
-      .collection('users')
+      .collection('users', this.filterFirstName.bind(this)  )
       .valueChanges({idField: 'customIdName'})
       .subscribe((changes: any) => {
         // console.log('Received all Users changes from DB', changes);
